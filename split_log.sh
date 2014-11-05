c53b8a3aef22d4d29654a024cac2ca2ec6502e22
mobliyishengyuan <moliyishengyuan@126.com>
2014-11-05 22:27:21 +0800

    init

---
+++ split_log.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+LOG_DIR_PATH=/home/work/log
+NGINX_PID=/home/work/webserver/logs/nginx.pid
+PHP_FPM_PID=/home/work/php/var/php-fpm.pid
+
+YESTERDAY=$(date -d last-day +%Y%m%d)
+
+find $LOG_DIR_PATH -name '*.log' -exec mv "{}" "{}"${YESTERDAY} \;
+
+kill -USR1 `cat $NGINX_PID`
+kill -USR1 `cat $PHP_FPM_PID`
